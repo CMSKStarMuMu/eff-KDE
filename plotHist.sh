3612db0c787e3c26eb190999da3877c89b2a6ee6
aboletti <boletti@pd.infn.it>
2019-10-31 00:49:24 +0100

    Added automated script for plotHist and plotEff steps, and removed absolute path from macros

---
+++ plotHist.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+par=1
+
+# Create directories
+if [ ! -d logs_plot ]; then mkdir logs_plot; fi
+if [ ! -d plotHist_d ]; then mkdir plotHist_d; fi
+
+while read -a line; do
+    
+    bin=${line[0]}
+    
+    for indx in {0..4}; do
+
+	# Plot KDE histograms
+	root -b -q 'plotHist.cc('${bin}','${indx}','${par}')' \
+	    &> logs_plot/plotHist_${bin}_${indx}_${par}.out &
+	
+    done
+
+done < ../confSF/KDE_SF.list
