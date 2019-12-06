a932602c324c32d8a376f840aa5af71749baa633
sara <sara.fiorendi@cern.ch>
2019-12-06 12:02:55 +0100

    address alessio's comments

--- run_composeEff_rooKeys.sh
+++ run_composeEff_rooKeys.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-export HOME=/afs/cern.ch/work/f/fiorendi/private/effKDE/eff-KDE
-export CMSSWDIR=/afs/cern.ch/work/f/fiorendi/private/effKDE/CMSSW_10_4_0/src
-export WORKDIR=$PWD
-
-cd $CMSSWDIR
-source  /cvmfs/cms.cern.ch/cmsset_default.sh
-eval `scram runtime -sh`
-
-cd $WORKDIR
-
 bin=${1}
 indx=${2}
 par=${3}
@@ -23,12 +13,21 @@ ndiv=${10}
 totdiv=${11}
 year=${12}
 
+export HOME=/afs/cern.ch/work/f/fiorendi/private/effKDE/eff-KDE
+export CMSSWDIR=/afs/cern.ch/work/f/fiorendi/private/effKDE/CMSSW_10_4_0/src
+export WORKDIR=$PWD
 export SAMPLEDIR=/eos/cms/store/user/fiorendi/p5prime/effKDE/${year}/lmnr/
+
+cd $CMSSWDIR
+source  /cvmfs/cms.cern.ch/cmsset_default.sh
+eval `scram runtime -sh`
+
+cd $WORKDIR
 
 echo 'now submitting for bin ' ${bin}
 
-if [ ! -r $SAMPLEDIR/effDataset_b${bin}.root ]; then
-    echo $SAMPLEDIR/effDataset_b${bin}.root not found
+if [ ! -r $SAMPLEDIR/effDataset_b${bin}${year}.root ]; then
+    echo $SAMPLEDIR/effDataset_b${bin}_${year}.root not found
     exit 1
 fi
 if [ ! -r $HOME/composeEff_rooKeys_parSub.cc ]; then
